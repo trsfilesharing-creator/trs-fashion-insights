2cdc92d19b99a5a99f65ca3bf567f04cf5dc6830
Nesara <nesaraa25@gmail.com>
2026-08-23 14:07:17 +0530

    Fix Render port binding

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const nodemailer = require("nodemailer");
 // =====================================================
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 // =====================================================
 // WHATSAPP WEB
 // =====================================================
@@ -1227,9 +1227,9 @@ app.get("/auth/logout", (req, res) => {
     // START EXPRESS SERVER
     // =================================================
 
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
-    });
+   app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (error) {
     console.error("MongoDB connection failed:", error);
   }
